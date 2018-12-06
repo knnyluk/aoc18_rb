f2ca78e6d146e7c63705863972cc39a1662dd2de
Ken Luk <knnyluk@gmail.com>
2018-12-06 17:51:40 -0500

    adding 2b solution

---
+++ 2/2b.rb
@@ -0,0 +1,30 @@
+def single_char_diff?(str_1, str_2)
+  seen_diff_char = false
+  str_2_chars = str_2.chars
+
+  str_1.each_char.with_index do |c, i|
+    if c != str_2_chars[i]
+      if seen_diff_char
+      	return false 
+      else
+      	seen_diff_char = true
+      end
+    end
+  end
+  true
+end
+
+def common_chars_str(str_1, str_2)
+  str_2_chars = str_2.chars
+
+  str_1.chars
+  	.select.with_index { |c, i| c == str_2_chars[i] }
+  	.join
+end
+
+File.readlines('input.txt').combination(2).each do |box_id_1, box_id_2|
+  if single_char_diff?(box_id_1, box_id_2)
+    puts "The common letters are: " + common_chars_str(box_id_1, box_id_2)
+    break
+  end
+end
