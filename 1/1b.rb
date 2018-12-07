1b7bbfd5ff2244a073992a37e9c864f8f6e8fea4
Ken Luk <knnyluk@gmail.com>
2018-12-07 06:32:12 -0500

    refactoring 1b

--- 1/1b.rb
+++ 1/1b.rb
@@ -4,13 +4,13 @@ input = File.readlines('input.txt').map(&:to_i)
 
 seen_frequencies = SortedSet.new
 current_freq = 0
-i = 0
+# i = 0
 
-loop do
-  current_freq += input[i]
+input.cycle do |change|
+  current_freq += change
   break if seen_frequencies.include? current_freq
   seen_frequencies.add current_freq
-  i = input[i + 1] ? i + 1 : 0
+  # i = input[i + 1] ? i + 1 : 0
 end
 
 puts "First repeated frequency is #{current_freq}"
